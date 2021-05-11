a3931cab70877c3d5690fba972997b8bd56636c7
Nurun Nabi <nnabi.cse@gmail.com>
2021-05-12 04:06:49 +0600

    Add files via upload

---
+++ strings_lesson12/strip()_method_strings.py
@@ -0,0 +1,9 @@
+def main():
+    x = '*****Welcome** To Guru*****'
+    y = x.strip('*')
+    print(y)
+# strip() only removes characters form the start and the end
+# not from inside the string.
+
+if __name__ == '__main__':
+    main()
