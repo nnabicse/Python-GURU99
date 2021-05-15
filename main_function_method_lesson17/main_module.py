f8ff1b2f3a5e66a153d30222f5061fd322845504
Nurun Nabi <nnabi.cse@gmail.com>
2021-05-16 02:24:32 +0600

    Add files via upload

---
+++ main_function_method_lesson17/main_module.py
@@ -0,0 +1,5 @@
+def main():
+    print('Guru 99')
+
+if __name__ == '__main__':
+    main()
---
+++ main_function_method_lesson17/name_variable.py
@@ -0,0 +1,9 @@
+def main():
+    print('Guru99')
+
+if __name__ == '__main__':
+    main()
+
+print('guru99')
+
+print(__name__)
