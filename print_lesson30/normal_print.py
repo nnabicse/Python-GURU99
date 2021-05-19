b63fec8e986fbab940d4e652611c238b100a1a68
Nurun Nabi <nnabi.cse@gmail.com>
2021-05-20 03:46:17 +0600

    Add files via upload

---
+++ print_lesson30/normal_print.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hello')
+    print('East Delta University')
+
+if __name__ == '__main__':
+    main()
---
+++ print_lesson30/without_newline_print.py
@@ -0,0 +1,5 @@
+def main():
+    print('Hello',end=' ')
+    print('East Delta University')
+if __name__ == '__main__':
+    main()
